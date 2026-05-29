9e8c4e1c917ab9d1fb204a2dca525b9c5b3d0abb
Chianese, Gino <chianese.gino@study.thws.de>
2026-05-29 19:50:07 +0200

    Updated name in about_screen.dart

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -8,7 +8,7 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('About BookHero'),
+        title: const Text('About BookSwap'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
